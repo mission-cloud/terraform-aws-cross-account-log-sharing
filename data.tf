195d8e4f3bd8d6d1a83e4294f91509ed8a21b98c
Alvaro Del Valle <adelvalle@missioncloud.com>
2021-10-08 12:02:02 -0400

    update iam policy for firehose to s3

--- data.tf
+++ data.tf
@@ -61,9 +61,23 @@ data "aws_iam_policy_document" "firehose_s3" {
   statement {
     sid       = "AllowFirehoseS3"
     effect    = "Allow"
-    resources = ["*"]
-    actions = [
-      "*"
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.this.id}",
+      "arn:aws:s3:::${aws_s3_bucket.this.id}/*"
     ]
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:PutObject"
+    ]
+  }
+
+  statement {
+    sid = "Logs"
+    effect = "Allow"
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:*:log-stream:*"]
   }
 }
